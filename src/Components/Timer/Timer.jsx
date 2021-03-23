8a4870bc299ddc781ce73974d53f0710e3075a76
Declan Bernardin <dmjbernardin@yahoo.com>
2021-03-23 15:21:15 -0500

    buttons are functional and dom displays the time in seconds and resets at 60

--- src/Components/Timer/Timer.jsx
+++ src/Components/Timer/Timer.jsx
@@ -3,11 +3,34 @@ import React, { Component } from 'react'
 
 class Timer extends Component {
 
+    componentDidMount() {
+        
+
+    }
     state = {
         timerOn: false,
         timeSeconds: 0,
         timeMinutes: 0,
-        timeHours: 0
+        timeHours: 0,
+        intervalId: 0
+    }
+
+    increment() {
+        let self = this
+        let num = self.state.timeSeconds + 1
+
+        if (self.state.timeSeconds < num) {
+            self.setState({
+                ...self.state, timeSeconds: num
+            })
+        } 
+        if (self.state.timeSeconds === 60) {
+            self.setState({
+                ...self.state, 
+                timeSeconds: 0
+            })
+        }
+        console.log(self.state.timeSeconds)
     }
 
 
@@ -16,42 +39,37 @@ class Timer extends Component {
             this.setState({
                 timerOn: true,
             })
-             setInterval(() => { 
-                 let num = this.state.timeSeconds++
-                 
-                if (this.state.timeSeconds < num) {
-                    this.setState({
-                        ...this.state, timeSeconds: num
-                    })
-                } else if (this.state.timeSeconds === 60) {
-                    this.setState({
-                        ...this.state, timeSeconds: 0
-                    })
-                }
-                console.log('this is state', this.state.timeSeconds)
-                 
-                   
-             }, 1000);
+            this.state.intervalId = setInterval(this.increment.bind(this), 1000);
+            console.log('Timer started')
         } else {
             console.log('already on')
         }
+    };
+
+    PauseTimer = () => {
+        if (this.state.timerOn === true) {
+            clearInterval(this.state.intervalId)
+            console.log('Timer paused');
+        } else if (this.state.timerOn === false){
+            console.log('Timer has not started')
+        } else {
+            console.log('Timer already paused')
+        }
     }
-        
-       
-    
 
     StopTimer = () => {
         if (this.state.timerOn === true) {
+            clearInterval(this.state.intervalId)
             this.setState({
                 ...this.state, 
                 timerOn: false,
                 timeSeconds: 0,
             })
-            
+            console.log('Timer off');
         } else {
             console.log('already off')
         }
-    }
+    };
     render() {
         return (
             <div>
@@ -60,7 +78,7 @@ class Timer extends Component {
                 <button onClick={this.StartTimer}>
                     Start
                 </button>
-                <button>
+                <button onClick={this.PauseTimer}>
                     Pause
                 </button>
                 <button onClick={this.StopTimer}>
