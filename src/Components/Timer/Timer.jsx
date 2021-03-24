64edb4439f8315eb8c993dc0ad0445ccc181b7ec
Declan Bernardin <dmjbernardin@yahoo.com>
2021-03-24 14:17:27 -0500

    fixed small bugs with clock and added some css to make it inline with itself

--- src/Components/Timer/Timer.jsx
+++ src/Components/Timer/Timer.jsx
@@ -9,6 +9,7 @@ class Timer extends Component {
         tenSeconds: false,
         tenMinutes:false,
         tenHours: false,
+        timeMilliseconds: 0,
         timeSeconds: 0,
         timeMinutes: 0,
         timeHours: 0,
@@ -25,6 +26,18 @@ class Timer extends Component {
             });
         };
 
+        
+        
+        if (self.state.timeSeconds === 60) {
+            let min = self.state.timeMinutes + 1
+            self.setState({
+                ...self.state, 
+                timeSeconds: 0,
+                timeMinutes: min
+            });
+            console.log(self.state.timeMinutes, 'Min')
+        }; 
+
         if (self.state.timeSeconds >= 10) {
             self.setState({
                 tenSeconds: true
@@ -36,16 +49,19 @@ class Timer extends Component {
             })
             console.log(this.state.tenSeconds, 'ten seconds')
         }
+
         
-        if (self.state.timeSeconds === 60) {
-            let min = self.state.timeMinutes + 1
+
+        if (self.state.timeMinutes === 60) {
+            let hour = self.state.timeHours + 1
             self.setState({
-                ...self.state, 
+                ...self.state,
                 timeSeconds: 0,
-                timeMinutes: min
+                timeMinutes: 0,
+                timeHours: hour
             });
-            console.log(self.state.timeMinutes, 'Min')
-        }; 
+            console.log(self.state.timeHours, 'Hour')
+        };
 
         if (self.state.timeMinutes >= 10) {
             self.setState({
@@ -59,23 +75,12 @@ class Timer extends Component {
             console.log(this.state.tenMinutes, 'ten min')
         }
 
-        if (self.state.timeMinutes === 60) {
-            let hour = self.state.timeHours + 1
-            self.setState({
-                ...self.state,
-                timeSeconds: 0,
-                timeMinutes: 0,
-                timeHours: hour
-            });
-            console.log(self.state.timeHours, 'Hour')
-        };
-
         if (self.state.timeHours >= 10) {
             self.setState({
                 tenHours: true
             })
             console.log(this.state.tenHours, 'ten Hours')
-        } else if (self.state.timeHours < 10) {
+        } else if ( self.state.timeHours < 10) {
             self.setState({
                 tenHours: false
             })
@@ -92,6 +97,7 @@ class Timer extends Component {
                 timerPause: false
             });
             this.state.intervalId = setInterval(this.increment.bind(this), 1000);
+
             console.log('Timer started')
         } else {
             console.log('already on')
@@ -134,12 +140,14 @@ class Timer extends Component {
     render() {
         return (
             <div>
-                {this.state.tenMinutes ? <h1> {this.state.timeHours} </h1> : <h1> 0{this.state.timeHours} </h1>}
-
-                {this.state.tenMinutes ? <h1> {this.state.timeMinutes}</h1> : <h1> 0{this.state.timeMinutes} </h1>}
-
-                {this.state.tenSeconds ? <h1> {this.state.timeSeconds}</h1> : <h1> 0{this.state.timeSeconds}</h1> }
-                
+                <div className="Timer">
+                    {this.state.tenHours ? <h1> {this.state.timeHours} </h1> : <h1> 0{this.state.timeHours} </h1>}
+                    <h1>:</h1>
+                    {this.state.tenMinutes ? <h1> {this.state.timeMinutes}</h1> : <h1> 0{this.state.timeMinutes} </h1>}
+                    <h1>:</h1>
+                    {this.state.tenSeconds ? <h1> {this.state.timeSeconds}</h1> : <h1> 0{this.state.timeSeconds}</h1>}
+                </div>
+                <div className="buttons">
                 <button onClick={this.StartTimer}>
                     Start
                 </button>
@@ -149,6 +157,7 @@ class Timer extends Component {
                 <button onClick={this.StopTimer}>
                     Stop
                 </button>
+                </div>
             </div>
         )
     }
